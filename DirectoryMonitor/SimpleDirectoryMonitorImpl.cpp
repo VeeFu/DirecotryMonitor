7cf192957d326ef361a7d3997ff569542e8f918b
FORMALCHICKEN\vdrak <vdrake@gmail.com>
2024-09-21 14:05:55 -0400

    Implement SimpleDirectoryImpl::run()
    WaitForMultipleObjects can take a large number of handles and returns an index to the object that has gone to a signaled state. We'll use this to index into our vectors. One caveat noted in comments

--- DirectoryMonitor/SimpleDirectoryMonitorImpl.cpp
+++ DirectoryMonitor/SimpleDirectoryMonitorImpl.cpp
@@ -32,5 +32,29 @@ namespace DirectoryMonitor {
     }
 
     void SimpleDirectoryMonitorImpl::run() {
+
+        while (true) 
+        { 
+            dwWaitStatus = WaitForMultipleObjects( dwChangeHandles.size(), dwChangeHandles.data(), FALSE, INFINITE);
+
+            // NOTE: WAIT_TIMEOUT is defined with value 0x102
+            // The simple parallel vectors implementation will not function for the 258th subscriber
+            if (dwWaitStatus == WAIT_FAILED)  {
+                printf("\n ERROR: WaitForMultipleObjects failed.\n");
+                ExitProcess(GetLastError());
+            }
+            else if (dwWaitStatus == WAIT_TIMEOUT) {
+                printf("\nNo changes in the timeout period.\n");
+                break;
+            }
+
+            if (FindNextChangeNotification(dwChangeHandles[dwWaitStatus]) == FALSE) {
+                printf("\n ERROR: FindNextChangeNotification function failed.\n");
+                ExitProcess(GetLastError()); 
+            }
+            else {
+                callbacks[dwWaitStatus](paths[dwWaitStatus]);
+            }
+        }
     }
 }
